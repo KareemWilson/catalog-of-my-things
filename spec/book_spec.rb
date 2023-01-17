5e84976729b618a7d254a9ebd4889c176965ad92
Olivier-Kango <olivierkango@gmail.com>
2023-01-17 12:01:31 -0800

    Add the test for Book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,24 +3,18 @@ require_relative '../modules/book'
 RSpec.describe Book do
   context 'can_be_archived?()' do
     it 'shoud return true' do
-      book = Book.new('2012-01-01', true, 'olivier', 'bad')
+      book = Book.new('A', '2012-01-01', true, 'olivier', 'bad')
       expect(book.can_be_archived?).to be true
     end
 
     it 'shoud return true' do
-      book = Book.new('2022-01-01', true, 'olivier', 'bad')
+      book = Book.new('A', '2022-01-01', true, 'olivier', 'bad')
       expect(book.can_be_archived?).to be true
     end
 
     it 'shoud return false' do
-      book = Book.new('2022-01-01', true, 'olivier', 'good')
+      book = Book.new('A', '2022-01-01', true, 'olivier', 'good')
       expect(book.can_be_archived?).to be false
     end
-
-    # it 'shoud return true' do
-    #   Item.new('2012/01/01', true)
-    #   result = Book.new('olivier', 'good')
-    #   expect(result.can_be_archived?).to be true
-    # end
   end
 end
