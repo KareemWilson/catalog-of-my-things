b418866c51328cc99d69fa3acd77a04ec75fb153
KareemWilson <kareemmagdywilson@gmail.com>
2023-01-18 15:07:29 +0200

    Operation methods for author and game classes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,12 +3,12 @@ require_relative '../modules/item'
 RSpec.describe Item do
   context 'move_to_archive' do
     it 'should return true' do
-      result = Item.new('2012/01/01', true)
+      result = Item.new('2012/01/01', false)
       expect(result.move_to_archive?).to be true
     end
 
     it 'should return true or false' do
-      result = Item.new('2022/01/01', true)
+      result = Item.new('2022/01/01', false)
       expect(result.move_to_archive?).to be nil
     end
   end
