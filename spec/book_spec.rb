c766db61195ff502db90daeebbe60c364f8e76db
Olivier-Kango <olivierkango@gmail.com>
2023-01-18 09:52:38 -0800

    Remove label

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,17 +3,17 @@ require_relative '../modules/book'
 RSpec.describe Book do
   context 'can_be_archived?()' do
     it 'shoud return true' do
-      book = Book.new('A', '2012-01-01', true, 'olivier', 'bad')
+      book = Book.new('2012-01-01', true, 'olivier', 'bad')
       expect(book.can_be_archived?).to be true
     end
 
     it 'shoud return true' do
-      book = Book.new('A', '2022-01-01', true, 'olivier', 'bad')
+      book = Book.new('2022-01-01', true, 'olivier', 'bad')
       expect(book.can_be_archived?).to be true
     end
 
     it 'shoud return false' do
-      book = Book.new('A', '2022-01-01', true, 'olivier', 'good')
+      book = Book.new('2022-01-01', true, 'olivier', 'good')
       expect(book.can_be_archived?).to be false
     end
   end
